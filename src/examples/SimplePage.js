cf9fa1f9d9a019b4f1889710de43847cc721a19c
Tujger <tujger@gmail.com>
2020-02-19 00:05:47 -0500

    About; Edeqa core; Home

--- src/examples/SimplePage.js
+++ src/examples/SimplePage.js
@@ -1,14 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const SimplePage = props => <div>
-  <h1>{props.title || "Simple page"}</h1>
-  {props.body || "Content of simple page"}
-</div>;
+const SimplePage = props => {
+  let body = props.body || "Content of simple page";
+  if(body instanceof Array) {
+    body = "<p>" + body.join("</p>\n<p>") + "</p>";
+  }
+  return <div>
+    <h1>{props.title || "Simple page"}</h1>
+    <div dangerouslySetInnerHTML={{__html: body}}/>
+  </div>;
+}
 
 SimplePage.propTypes = {
   title: PropTypes.string,
-  body: PropTypes.string,
+  body: PropTypes.array | PropTypes.string,
 };
 
 export default SimplePage;
