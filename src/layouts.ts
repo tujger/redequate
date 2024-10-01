c537212055f15e35a731210869c89d680d995100
Tujger <tujger@gmail.com>
2024-09-30 22:14:31 -0400

    Convert index.js to layouts.ts for better module organization.

---
+++ src/layouts.ts
@@ -0,0 +1,3 @@
+export {default as ResponsiveDrawerLayout} from "./layouts/ResponsiveDrawerLayout/ResponsiveDrawerLayout.js";
+export {default as TopBottomMenuLayout} from "./layouts/TopBottomMenuLayout/TopBottomMenuLayout.js";
+export {default as BottomToolbarLayout} from "./layouts/BottomToolbarLayout/BottomToolbarLayout.js";
--- src/layouts/index.js
+++
@@ -1,3 +0,0 @@
-export {default as ResponsiveDrawerLayout} from "./ResponsiveDrawerLayout/ResponsiveDrawerLayout";
-export {default as TopBottomMenuLayout} from "./TopBottomMenuLayout/TopBottomMenuLayout";
-export {default as BottomToolbarLayout} from "./BottomToolbarLayout/BottomToolbarLayout";
