b24750b077f4f3167107fa66aee7268ccdcf7bd5
Tujger <tujger@gmail.com>
2026-09-24 13:20:00 -0400

    Update package version and add new exports for posts
    Bumped package version to 1.2.17 and adjusted publish configuration for public registry access. Added `NewPost` and `Post` exports to improve page functionality. Updated relevant dependencies and scripts accordingly.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ export {default as Login} from "./Login";
 export {default as Logout} from "./Logout";
 export {default as EditProfile} from "./EditProfile";
 export {default as Home} from "./Home";
+export {default as NewPost} from "./NewPost";
+export {default as Post} from "./Post";
 export {default as NotFound} from "./NotFound";
 export {default as Profile, publicFields} from "./Profile";
 export {default as RestorePassword} from "./RestorePassword";
