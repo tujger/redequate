5f2b9ec1d9e021beac4cc09e75f045deaa11742f
Tujger <tujger@gmail.com>
2026-09-26 01:54:30 -0400

    Modularize Chats styles and replace Material-UI components with CSS modules
    Refactored `Chats.js` to use `Chats.module.css` for layouts. Removed Material-UI components (Grid, withStyles) and replaced them with flexbox-based layouts. Simplified logic, removed unused legacy code, and improved maintainability by aligning with the modular architecture.

--- src/chat/Chats.js
+++ src/chat/Chats.js
@@ -1,36 +1,23 @@
 import React from "react";
-import {useDispatch} from "react-redux";
-import withStyles from "@material-ui/styles/withStyles";
-import {useCurrentUserData} from "../controllers/UserData";
-import LazyListComponent from "../components/LazyListComponent/LazyListComponent";
-import Pagination from "../controllers/FirebasePagination";
-import ChatsItem from "./ChatsItem";
-import ChatsDaemon from "./ChatsDaemon";
-import {lazyListComponentReducer} from "../components/LazyListComponent/lazyListComponentReducer";
-import Grid from "@material-ui/core/Grid";
-import {styles} from "../controllers/Theme";
-import Chat from "./Chat";
 import {useTranslation} from "react-i18next";
+import {useDispatch} from "react-redux";
+import LazyListComponent from "../components/LazyListComponent/LazyListComponent";
+import {lazyListComponentReducer} from "../components/LazyListComponent/lazyListComponentReducer";
+import Pagination from "../controllers/FirebasePagination";
+import {useCurrentUserData} from "../controllers/UserData";
+import ChatsDaemon from "./ChatsDaemon";
+import ChatsItem from "./ChatsItem";
+import chatStyles from "./styles/Chats.module.css";
 
-const stylesCurrent = theme => ({
-    observer: {
-        height: 0,
-        width: "100%",
-    },
-});
-
-function Chats(
+export default (
     {
-        classes,
         daemon,
         textComponent = text => text,
         userComponent = userData => userData.name,
         ...rest
-    }) {
+    }) => {
     const currentUserData = useCurrentUserData();
     const dispatch = useDispatch();
-    const [state, setState] = React.useState({});
-    const {chatId} = state;
     const {t} = useTranslation();
 
     React.useEffect(() => {
@@ -47,72 +34,28 @@ function Chats(
         />
     }
 
-    // if(windowData.isNarrow()) {
-        return <Grid container className={classes.center}>
-            <LazyListComponent
-                cache={"chats"}
-                itemComponent={item => <ChatsItem
-                    id={item.key}
-                    key={item.key}
-                    lastMessageTimestamp={item.value.timestamp}
-                    textComponent={textComponent}
-                    userComponent={userComponent}
-                />}
-                itemTransform={async item => {
-                    if (item.key === "!meta") return null;
-                    return item
-                }}
-                noItemsComponent={<ChatsItem label={t("Chat.No chats found")}/>}
-                pagination={() => new Pagination({
-                    child: "timestamp",
-                    order: "desc",
-                    start: 0,
-                    ref: "_chats/" + currentUserData.id,
-                })}
-                placeholder={<ChatsItem skeleton/>}
-            />
-        </Grid>
-    // }
-
-    return <Grid container className={classes.center}>
-        <Grid container>
-            <Grid item>
-                <LazyListComponent
-                    cache={"chats"}
-                    itemComponent={item => <ChatsItem
-                        id={item.key}
-                        key={item.key}
-                        lastMessageTimestamp={item.value.timestamp}
-                        onClick={chatId => {
-                            setState(state => ({...state, chatId}))
-                        }}
-                        textComponent={textComponent}
-                        userComponent={userComponent}
-                    />}
-                    itemTransform={async item => {
-                        if (item.key === "!meta") return null;
-                        return item
-                    }}
-                    noItemsComponent={<ChatsItem label={t("Chat.No chats found")}/>}
-                    pagination={() => new Pagination({
-                        child: "timestamp",
-                        start: 0,
-                        order: "desc",
-                        ref: "_chats/" + currentUserData.id,
-                    })}
-                    placeholder={<ChatsItem skeleton/>}
-                />
-            </Grid>
-            <Grid item xs>
-                <Chat
-                    id={chatId}
-                />
-            </Grid>
-        </Grid>
-    </Grid>
+    return <div className={chatStyles.center}>
+        <LazyListComponent
+            cache={"chats"}
+            itemComponent={item => <ChatsItem
+                id={item.key}
+                key={item.key}
+                lastMessageTimestamp={item.value.timestamp}
+                textComponent={textComponent}
+                userComponent={userComponent}
+            />}
+            itemTransform={async item => {
+                if (item.key === "!meta") return null;
+                return item
+            }}
+            noItemsComponent={<ChatsItem label={t("Chat.No chats found")}/>}
+            pagination={() => new Pagination({
+                child: "timestamp",
+                order: "desc",
+                start: 0,
+                ref: "_chats/" + currentUserData.id,
+            })}
+            placeholder={<ChatsItem skeleton/>}
+        />
+    </div>
 }
-
-export default withStyles(theme => ({
-    ...styles(theme),
-    ...stylesCurrent(theme)
-}))(Chats);
