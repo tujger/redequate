0bd1e89ff7055b16c69cca2c70efc23fb3c701f0
Tujger <tujger@gmail.com>
2026-09-26 00:44:28 -0400

    Modularize ChatItem styles and replace Material-UI components with CSS modules
    Replaced Material-UI Card, Grid, and withStyles with div-based layouts styled via CSS modules (`ChatItem.module.css`). Updated `Base.module.css` with a new theme variable (`--theme-color-paper-alt`). Simplified `ChatItem.js` structure and removed unused legacy styles for improved maintainability.

--- src/chat/ChatItem.js
+++ src/chat/ChatItem.js
@@ -1,55 +1,21 @@
 import React from "react";
-import withStyles from "@material-ui/styles/withStyles";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import Grid from "@material-ui/core/Grid";
 import {cacheDatas, toDateString, useCurrentUserData, UserData} from "../controllers";
 import AvatarView from "../components/AvatarView";
 import ItemPlaceholderComponent from "../components/ItemPlaceholderComponent";
-import {stylesList} from "../controllers/Theme";
-
-const stylesChat = theme => ({
-    chatItem: {
-        marginBottom: theme.spacing(0.5),
-        width: "90%",
-        "& $cardHeader": {
-            padding: 0,
-        }
-    },
-    chatItemOut: {
-        marginLeft: "10%",
-        "& $cardContent": {
-            backgroundColor: "#def6fc",
-        }
-    },
-    cardContent: {},
-    cardHeader: {},
-    chatItemIn: {
-        marginRight: "10%",
-    },
-    _text: {
-        marginBottom: theme.spacing(1),
-    },
-    timestamp: {
-        bottom: theme.spacing(0.5),
-        fontSize: theme.spacing(1.25),
-        position: "absolute",
-        right: theme.spacing(1),
-    },
-});
+import chatStyles from "./styles/ChatItem.module.css";
 
 const ChatItem = (props) => {
     // eslint-disable-next-line react/prop-types
-    const {data, classes, skeleton, textComponent} = props;
+    const {data, skeleton, textComponent} = props;
     const currentUserData = useCurrentUserData();
     const [state, setState] = React.useState({});
     const {authorData} = state;
 
-    /*const fetchIsNew = () => {
+    /* const fetchIsNew = () => {
         const latestVisit = chatMeta[currentUserData.id + "_visit"] || 0;
         // const latestIncoming = meta[currentUserData.id] || 0;
         return data.created > latestVisit;
-    }*/
+    } */
 
     React.useEffect(() => {
         if (skeleton) return;
@@ -67,38 +33,28 @@ const ChatItem = (props) => {
         // eslint-disable-next-line
     }, []);
 
-    if (skeleton) return <ItemPlaceholderComponent classes={{avatar: classes.avatarChat}}/>;
+    if (skeleton) return <ItemPlaceholderComponent/>;
 
     // const isNew = fetchIsNew() && !shown;
     const isItemOut = currentUserData.id === data.uid;
 
     if (!authorData) return null;
-    return <Card className={[
-        classes.root,
-        classes.card,
-        classes.cardCloud,
-        classes.chatItem,
-        isItemOut ? classes.chatItemOut : classes.chatItemIn
-    ].join(" ")}>
-        <CardHeader
-            classes={{content: classes.cardContent}}
-            className={[classes.cardHeader, classes.post].join(" ")}
-            avatar={<AvatarView
-                className={[classes.avatar, classes.avatarSmallest].join(" ")}
+    return <div className={[chatStyles.chatItem, isItemOut ? chatStyles.chatItemOut : chatStyles.chatItemIn].join(" ")}>
+        <div className={chatStyles.messageContent}>
+            <AvatarView
+                className={chatStyles.avatarSmallest}
                 image={authorData.image}
                 initials={authorData.initials}
                 verified={true}
-            />}
-            title={<Grid container>
-            </Grid>}
-            subheader={<Grid container className={classes._text}>
-                <Grid item xs>{textComponent(data.text)}</Grid>
-                <Grid className={[classes.date, classes.timestamp].join(" ")}>{toDateString(data.created)}</Grid>
-            </Grid>}
-        />
-    </Card>
+            />
+            <div className={chatStyles.textWrapper}>
+                <div className={chatStyles.text}>{textComponent(data.text)}</div>
+                <div className={chatStyles.timestamp}>{toDateString(data.created)}</div>
+            </div>
+        </div>
+    </div>
 
-    /*return <li>
+    /* return <li>
         {isNew && <InView
             onChange={(inView) => {
                 if (inView) setState({...state, shown: true});
@@ -111,10 +67,7 @@ const ChatItem = (props) => {
             }}
             style={{display: "none"}}
         ><b>NEW</b></InView>}
-    </li>*/
+    </li> */
 }
 
-export default withStyles((theme) => ({
-    ...stylesList(theme),
-    ...stylesChat(theme),
-}))(ChatItem);
+export default ChatItem;
