1e4e0a6aded39a3dfcf63825b42ad094e7704965
Tujger <tujger@gmail.com>
2026-09-25 23:18:13 -0400

    Modularize AlertsCounter styles and remove Material-UI withStyles usage

--- src/alerts/AlertsCounter.js
+++ src/alerts/AlertsCounter.js
@@ -1,15 +1,14 @@
 import React from "react";
 import {connect} from "react-redux";
-import {styles} from "../controllers/Theme";
-import withStyles from "@material-ui/styles/withStyles";
+import alertCounterStyles from "./styles/AlertsCounter.module.css";
 
-const AlertsCounter = ({counter, classes}) => {
+const AlertsCounter = ({counter}) => {
     if (!counter) return null;
-    return <span className={classes.badge}>{counter}</span>
+    return <span className={alertCounterStyles.badge}>{counter}</span>
 }
 
 const mapStateToProps = ({alertsCounter}) => ({
     counter: alertsCounter.counter,
 });
 
-export default connect(mapStateToProps)(withStyles(styles)(AlertsCounter));
+export default connect(mapStateToProps)(AlertsCounter);
